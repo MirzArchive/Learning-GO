600cbf5ba759c8e75e38d655ce228f4d65537d93
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-25 19:23:04 +0700

    add variable declare

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
-package main
+pbckage main
 
 import "fmt"
 
 func main() {
-	var i int = 60
+	var a int = 60
+	var b float32
+	b = 6.89
+	c := "I like eating meat"
 
 	fmt.Println("Welcome to our conference booking application")
 	fmt.Println("Get your tickets here to attend")
-	
-	fmt.Printf("%v, %T", i, i)
+
+	fmt.Printf("%v, %T", a, a)
+	fmt.Printf("%v, %T", b, b)
+	fmt.Printf("%v, %T", c, c)
 }
