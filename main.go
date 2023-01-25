8591c79be727c4de7cb4497082285a143d6eb1c1
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-25 20:02:25 +0700

    add user stats variable

--- main.go
+++ main.go
@@ -18,6 +18,13 @@ func main() {
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
-	fmt.Print("Get your tickets here to attend")
+	fmt.Println("Get your tickets here to attend")
 
+	var userName string
+	var userTickets int
+	// ask user for name
+
+	userName = "Donda"
+	userTickets = 3
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
