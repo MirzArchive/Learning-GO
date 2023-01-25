fd95aff5fbfc36409bca99f7fad2ef65681f2ebb
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-25 20:36:17 +0700

    add additional user data and display it in terminal

--- main.go
+++ main.go
@@ -20,12 +20,23 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 	// ask user for name
 
 	fmt.Print("Enter your first name: ")
-	fmt.Scan(&userName) // Passing the reference
-	userTickets = 3
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Scan(&firstName) // Passing the reference
+
+	fmt.Print("\nEnter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Print("\nEnter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Print("\nEnter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v", firstName, lastName, userTickets, email)
 }
