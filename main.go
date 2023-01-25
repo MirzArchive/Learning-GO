79900314976fb90606a5d0b920d89fcb887969ae
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-25 19:21:18 +0700

    add print syntax to check data type

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	var i int = 60
+
 	fmt.Println("Welcome to our conference booking application")
 	fmt.Println("Get your tickets here to attend")
+	
+	fmt.Printf("%v, %T", i, i)
 }
