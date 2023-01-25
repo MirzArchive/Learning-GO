86c7807157c9819909e47c2a7b1d8e6a2a9166e6
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-25 19:52:41 +0700

    change unused code to comment

--- main.go
+++ main.go
@@ -3,21 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var a int = 60
+	/*var a int = 60
 	var b float32
 	b = 6.89
 	c := "I like eating meat"
 
 	fmt.Printf("%v, %T\n", a, a)
 	fmt.Printf("%v, %T\n", b, b)
-	fmt.Printf("%v, %T\n\n", c, c)
+	fmt.Printf("%v, %T\n\n", c, c)*/
 
 	var conferenceName string = "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets int = conferenceTickets
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Println("We have total of", conferenceTickets, " tickets and", remainingTickets, "are still available")
-	fmt.Println("Get your tickets here to attend")
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Print("Get your tickets here to attend")
 
 }
