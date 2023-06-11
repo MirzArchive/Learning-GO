4d461167cf1409fc05245fb7574a9476afba2055
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-06-11 23:13:46 +0700

    `feat:` Add simple cli to run exercise code

--- main.go
+++ main.go
@@ -2,14 +2,41 @@ package main
 
 import (
 	"fmt"
-
-	"golang.org/x/exp/constraints"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"runtime"
 )
 
-func sum[T constraints.Ordered](a T, b T) T {
-	return a + b
-}
+// func sum[T constraints.Ordered](a T, b T) T {
+// 	return a + b
+// }
 
 func main() {
-	fmt.Println(sum(1.1, 2.2))
+	file := os.Args[1:2]
+
+	var err error
+	var output []byte
+
+	pkg := fmt.Sprintf(
+		"go run %s",
+		path.Join(
+			"exercises",
+			"noval_agung",
+			file[0],
+			"main.go"),
+	)
+
+	if runtime.GOOS == "windows" {
+		output, err = exec.Command("cmd", "/C", pkg).Output()
+	} else {
+		output, err = exec.Command("bsdh", "-c", pkg).Output()
+	}
+
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	fmt.Println(string(output))
 }
